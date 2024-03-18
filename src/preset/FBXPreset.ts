05816e499e6f88dc28f19d729f06a0d1648e61fb
김명준 <acspp2017@gmail.com>
2024-03-18 18:31:59 +0900

    add preset

---
+++ src/preset/FBXPreset.ts
@@ -0,0 +1,13 @@
+import { ViewerOptions } from "../viewer.type.ts";
+
+export const PolygroundPreset: ViewerOptions = {
+  background: false,
+  autoRotate: true,
+  ambientIntensity: 0.3,
+  ambientColor: "#FFFFFF",
+  directIntensity: 0.8 * Math.PI,
+  directColor: "#FFFFFF",
+  bgColor: "#191919",
+  setBaseColor: true,
+  BaseColor: "0x696969",
+};
